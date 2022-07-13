3a971872ae7e1a3856800b4ed2d4f3fed32adcac
Jonas Lieske <j.lieske@student.fontys.nl>
2022-07-13 15:22:04 +0200

    Fixed hide_metadata

--- utils/hide_metadata.js
+++ utils/hide_metadata.js
@@ -20,11 +20,17 @@ let item = data[0];
 if (network == NETWORK.sol) {
   item.name = `Unrevealed ${namePrefix}`;
   item.description = description;
+  item.dna = placeHolderText;
+  item.edition = 0;
 } else {
   item.name = `Unrevealed ${namePrefix}`;
   item.description = description;
   item.image = `${hiddenImageUri}`;
+  item.dna = placeHolderText;
+  item.edition = 0;
 }
+
+item.attributes = {};
 
 fs.writeFileSync(
     `${basePath}/build/hidden.json`,
@@ -35,12 +41,14 @@ if (network == NETWORK.sol) {
   console.log(`Updated description for images to ===> ${placeHolderText}`);
   console.log(`Updated name prefix for images to ===> ${placeHolderText}`);
   console.log(
-    `Updated creators for images to ===> ${JSON.stringify(
-      solanaMetadata.creators
-    )}`
+      `Updated creators for images to ===> ${JSON.stringify(
+          solanaMetadata.creators
+      )}`
   );
 } else {
+  console.log(`Updated description to ===> ${description}`);
+  console.log(`Updated name prefix to ===> Unrevealed ${namePrefix}`);
+  console.log(`Updated edition to ===> 0`);
   console.log(`Updated baseUri for images to ===> ${placeHolderText}`);
-  console.log(`Updated description for images to ===> ${placeHolderText}`);
-  console.log(`Updated name prefix for images to ===> ${placeHolderText}`);
+  console.log(`Updated dna to ===> ${placeHolderText}`);
 }
